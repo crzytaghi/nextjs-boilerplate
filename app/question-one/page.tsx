6d8198edd8bff6a30c19d4db4d6fbf38f6df2330
Andrew Taghi <aataghi13@gmail.com>
2025-01-14 10:15:26 -0600

    updated response for question one

--- app/question-one/page.tsx
+++ app/question-one/page.tsx
@@ -7,31 +7,21 @@ const QuestionOne = () => {
       <div style={responseStyles}>
         <h1>Question One</h1>
         <h1>
-          Compare the similarities and differences between Next.js and React
+          An Enterprise customer has come to you to ask about the difference
+          between React and Next.js and what the benefits are of using Next.js
+          for her team.
         </h1>
         <h2>
-          Similarities: Next.js is a javascript framework that is built on top
-          of react. That means that you can write your next.js components as you
-          would your react components using the same component based
-          architecture, JSX syntax, and react ecosystems. They also both utilize
-          the virtual DOM, only updating portions of the DOM that are being
-          changed, for more efficient page rendering.
-        </h2>
-        <h2>
-          Differences: React is a javascript library while next.js, built on top
-          of react, is a full-fledged framework for building web applications.
-          Next.js utilizes server side rendering (SSR), which means that the
-          HTML of the page is loaded by the server, allowing for faster page
-          loading and search engine optimization SEO. Next.js also provides in
-          house file-based routing that handles pre-fetching routes for better
-          optimization and efficiency loading pages whereas react does not
-          handle routing and requires third party libraries such as react-router
-          to handle routing. React also allows for mobile development through
-          React Native, which is an open source platform that handles optimizing
-          the code to render either native iOS or Android applications without
-          having to utilize Swift, Flutter, or Kotlin. Next.js also offers
-          code-splitting, which loads only the necessary javascript code for
-          each route which improves performance by reducing initial load times
+          Next.js provides a suite of differences between react that will
+          benefit you and your team. Specifically for SEO, Next.js utilizes
+          server side rendering, SSR, which essentially means that the content
+          of the page is being loaded by the backend server, which is much more
+          efficient and scores higher for SEO. Additional efficiency is realized
+          through code-splitting, which loads only the necessary javascript code
+          for each route which improves performance by reducing initial load
+          times. Next.js also optimizes the loading of third party libraries
+          like Google Tag Manager, which are important for tracking the
+          analytics and success of marketing campaigns.
         </h2>
         <div>
           <Link
